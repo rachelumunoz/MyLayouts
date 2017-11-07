87af3a95cec78bf58b50ff241656aadea9cb5d40
rachelmunoz <rachelumunoz>
2017-11-07 12:32:44 -0800

    Initial Commit

---
+++ app/src/main/java/io/rachelmunoz/mylayouts/LoginSignUpActivity.java
@@ -0,0 +1,13 @@
+package io.rachelmunoz.mylayouts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginSignUpActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.sign_in);
+	}
+}
